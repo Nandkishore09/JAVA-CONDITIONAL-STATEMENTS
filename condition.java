b884bf2d0456e87417c0f8c6411e3e9f36cba2ae
Nandkishore Suthar <sutharnandkishor05@gmail.com>
2026-07-27 10:59:31 +0530

    Add files via upload

---
+++ condition.java
@@ -0,0 +1,80 @@
+// to check two condition 
+
+// import java.util.Scanner;
+
+// public class condition{
+//     public static void main(String args[]){
+    
+//         Scanner sc = new Scanner(System.in);
+//         int age = sc.nextInt();
+
+//         if(age>18){
+//             System.out.println("adult");
+//         }
+//         else{
+//             System.out.println("NOt adult");
+//         }
+
+//     }
+
+// }
+// ---------------------------------------------------------------------
+//             print no even or odd 
+
+// import java.util.Scanner;
+
+// public class condition{
+//     public static void main(String args[]){
+//         Scanner sc= new Scanner(System.in);
+//         // int x = sc.nextInt();
+
+//         // if(x%2==0){
+//         //     System.out.println("Even No");
+//         // }
+//         // else{
+//         //     System.out.println("Odd No");
+//         // }
+
+        // ----------NO IS GREATER OR LESSOR ----------------
+
+//         int a =sc.nextInt();
+//         int b = sc.nextInt();
+
+//         if(a==b){
+//             System.out.println("equal");
+//         }
+//         else if(a>b){
+//             System.out.println("a is greater");
+
+//         }
+//         else{
+//             System.out.println("a is lesser");
+//         }
+//     }
+// }
+
+// -------------SWITCH OR BREAK CASE -----------
+
+// import java.util.Scanner;
+// class condition{
+//         public static void main (String args[]){
+//                 Scanner sc =new Scanner(System.in);
+//                 int button = sc.nextInt();
+
+//                 switch (button) {
+//                         case 1: System.out.println("hello");
+//                         break;
+
+//                         case 2: System.out.println("nameste");
+//                         break;
+
+//                         case 3: System.out.println("bonjour");
+//                         break;
+                
+//                         default: System.out.println("Invalid button ");
+//                                 break;
+//                 }
+
+//         }
+
+// }
